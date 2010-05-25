a37daa4de1d9e527a7e0c7eeb866359daa933a01
James Smith <jgsmith@tamu.edu>
2010-05-25 18:41:56 +0000

    Allow retrieval by id or name via api

--- app/controllers/api/exhibits_controller.rb
+++ app/controllers/api/exhibits_controller.rb
@@ -1,7 +1,7 @@
 class Api::ExhibitsController < ApplicationController
 
     def show
-      @exhibit = FabulatorExhibit.find(params[:id])
+      @exhibit = FabulatorExhibit.find(:first, :conditions => [ "id = ? OR name = ?", params[:id], params[:id] ])
       respond_to do |format|
         format.json { render :json => @exhibit.data }
       end
