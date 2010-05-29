35ffd3a70456b33df7aa795be196f5a6cfd41502
James Smith <jgsmith@tamu.edu>
2010-05-29 21:34:04 +0000

    Remove auth requirement for retrieving exhibits

--- app/controllers/api/exhibits_controller.rb
+++ app/controllers/api/exhibits_controller.rb
@@ -1,4 +1,7 @@
 class Api::ExhibitsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  no_login_required
 
     def show
       @exhibit = FabulatorExhibit.find(:first, :conditions => [ "name = ?", params[:id] ])
